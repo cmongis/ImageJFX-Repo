b8eb9d91b902d09286a575a666fdd90e2cb1719c
MitchConnor1 <ormagoden666.42@gmail.com>
2017-08-02 17:35:15 +0200

    Adding express.static functionnality

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    finalDatabase : 'public/db.xml',
+    pathToImageJDependencies : 'http://update.imagej.net/db.xml.gz',
+    pathToImageJFXDependencies : 'imagejfx-core/target/imagejfx-core-1.1-bin/imagejfx-core-1.1/lib/',
+    pathToImageJFX : 'imagejfx-core/target/imagejfx-core-1.1-bin/imagejfx-core-1.1/imagejfx-core-1.1.jar',
+    dependenciesDirectory : 'public/lib/',
+    assemblyScript : './assembly.sh',
+    port : 8080
+}
