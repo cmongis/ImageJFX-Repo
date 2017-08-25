1a54bbad03ce6a5955c42e4e09282171d232d5a9
cyril <cyril@129.206.96.102>
2017-08-25 16:28:10 +0200

    New update sequence using the Seq library

---
+++ test.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var updater = require("./updater2.js");
+var Seq = require("seq");
+var config = require("./config.js");
+
+
+Seq()   
+        // first we get the list of jars of the remote
+        .par(function () {
+            updater.getRemote(config.pathToImageJDependencies, this)
+        })
+        // then we the the list of jars that was compiled
+        .par(function (array) {
+            updater.getLocal(__dirname + "/" + config.pathToImageJFXDependencies, this);
+        })
+        // in case of error, we just print it
+        .catch(function (err) {
+            console.log(err);
+        })
+        // we copy the file to the destination
+        .seq(function(remote,local) {           
+            var toCopy = updater.compare(remote,local);       
+            toCopy(from,)
+        })
+        
+        ;
+
+
+
+
+
+
