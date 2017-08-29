08b4e416e5330085dce76d9b0b2f318810346939
cyril <cyril@129.206.96.102>
2017-08-29 15:22:19 +0200

    Finished up the test framework

--- test.js
+++ test.js
@@ -7,9 +7,33 @@
 var updater = require("./updater2.js");
 var Seq = require("seq");
 var config = require("./config.js");
+var exec = require("child_process").exec;
+
+var response = {
+
+    output: ""
+
+}
+
+Seq()
 
 
-Seq()   
+        .seq(function () {
+
+            //exec("bash assembly.sh", this);
+            this();
+
+        })
+        .catch(function (err, output) {
+            console.log(err, output)
+        })
+
+        .seq(function (result, output) {
+
+            //console.log(result, output);
+            response.output = output;
+            this();
+        })
         // first we get the list of jars of the remote
         .par(function () {
             updater.getRemote(config.pathToImageJDependencies, this)
@@ -18,25 +42,37 @@ Seq()
         .par(function (array) {
             updater.getLocal(__dirname + "/" + config.pathToImageJFXDependencies, this);
         })
-        
-        
+
+
         // in case of error, we just print it
         .catch(function (err) {
             console.log(err);
         })
         // we copy the file to the destination
-        .seq(function(remote,local) {
+        .seq(function (remote, local) {
+
+           
             
-            
-            console.log(JSON.stringify(remote.pluginRecords.plugin,null,4));
-            
-           /*
-            var toCopy = updater.compare(remote,local);
+            // we filter the local list of jar to contain only jars
+            local = local.filter(function (item) {
+
+                return item.indexOf(".jar") != -1;
+
+            });
+
+            //
+            var toCopy = updater.compare(remote, local);
             console.log(toCopy);
-            updater.copyList(config.pathToImageJDependencies,toCopy,config.dependenciesDirectory);
-            */
-            })
-        
+            updater.copyList(toCopy, config.pathToImageJFXDependencies, config.dependenciesDirectory);
+
+            updater.writeXMLFile("jars/", toCopy, "db.xml", this);
+
+        })
+        .seq(function () {
+
+            console.log("Build success");
+        })
+
         ;
 
 
