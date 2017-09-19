c3cd493ae39d2142a7956ae50e7f71dabfd36ff9
cyril <cyril@129.206.96.102>
2017-09-19 16:50:17 +0200

    Changes templates to use html extension insteand of handlebars extension

--- public/scripts/build.js
+++ public/scripts/build.js
@@ -1,13 +1,37 @@
-document.getElementById("buildButton").onclick = function () { 
-    
+var build = function () {
+
+    $("#buildButton")
+            .text("Building...");
+
+
+    var showData = function (data) {
+
+        $("#time").text(data.responseText);
+         $("#buildButton")
+            .text("Build");
+    };
+
+    $.ajax({
+        type: "POST"
+        , url: "/update"
+        , data:
+                {
+                    password: $("#password").val()
+                    , hello: "itsme"
+                }
+
+    })
+    .always(showData);
+    //.fail(showData);
+    return;
     var req = new XMLHttpRequest();
     req.open("GET", "/update", false);
     this.firstChild.data = "Building...";
     req.send(null);
     if (req.status === 200)
-	document.getElementById("time").firstChild.innerHTML = "Last Time Built: " + req.responseText;
+        document.getElementById("time").firstChild.innerHTML = "Last Time Built: " + req.responseText;
     else
-	document.getElementById("time").firstChild.innerHTML = "Error building: " + req.status + " " + req.statusText;
+        document.getElementById("time").firstChild.innerHTML = "Error building: " + req.status + " " + req.statusText;
     this.firstChild.data = "Build";
 };
 
