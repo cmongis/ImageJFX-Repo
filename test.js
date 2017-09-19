bd6e24b20b36e15e144df8ad95172c65d9412134
cyril <cyril@129.206.96.102>
2017-09-19 16:49:54 +0200

    Testing the new repository framework

--- test.js
+++ test.js
@@ -4,79 +4,21 @@
  * and open the template in the editor.
  */
 
-var updater = require("./updater2.js");
+
+
+
+
+var Repository = require("./repository.js");
+var xml2js = require("xml2js");
+var fs = require("fs");
 var Seq = require("seq");
-var config = require("./config.js");
-var exec = require("child_process").exec;
-
-var response = {
-
-    output: ""
-
-}
-
-Seq()
-
-
-        .seq(function () {
-
-            //exec("bash assembly.sh", this);
-            this();
-
-        })
-        .catch(function (err, output) {
-            console.log(err, output)
-        })
-
-        .seq(function (result, output) {
-
-            //console.log(result, output);
-            response.output = output;
-            this();
-        })
-        // first we get the list of jars of the remote
-        .par(function () {
-            updater.getRemote(config.pathToImageJDependencies, this)
-        })
-        // then we the the list of jars that was compiled
-        .par(function (array) {
-            updater.getLocal(__dirname + "/" + config.pathToImageJFXDependencies, this);
-        })
-
-
-        // in case of error, we just print it
-        .catch(function (err) {
-            console.log(err);
-        })
-        // we copy the file to the destination
-        .seq(function (remote, local) {
-
-           
-            
-            // we filter the local list of jar to contain only jars
-            local = local.filter(function (item) {
-
-                return item.indexOf(".jar") != -1;
-
-            });
-
-            //
-            var toCopy = updater.compare(remote, local);
-            console.log(toCopy);
-            updater.copyList(toCopy, config.pathToImageJFXDependencies, config.dependenciesDirectory);
-
-            updater.writeXMLFile("jars/", toCopy, "db.xml", this);
-
-        })
-        .seq(function () {
-
-            console.log("Build success");
-        })
-
-        ;
-
-
-
-
-
-
+var zlib = require("zlib");
+var repo = new Repository("./", "./jars");
+repo    .read(function(db) {
+        
+        
+        //console.log(repo.getId("jars/ijfx-core-1.1.jar"));
+        //console.log(repo.getPlugin("imagejfx-core"));
+        //repo.checkCurrentJars();
+        repo.write();
+});
