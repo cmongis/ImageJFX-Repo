8114297ab028809fdd99264eec459b449211451e
cyril <cyril@129.206.96.102>
2017-09-20 16:36:55 +0200

    now uses the new repository.js implementation to generate the db.xml.gz

--- updater2.js
+++ updater2.js
@@ -12,57 +12,60 @@ var exec = require("child_process").exec;
 
 var spawn = require("child_process").spawn;
 
+var Repository = require("./repository.js");
 
-module.exports = function (callback,progress) {
+var repo = new Repository(__dirname);
+
+module.exports = function (callback, progress) {
     var response = {
         sdtout: ""
-        ,stderr:""
+        , stderr: ""
     };
-    
-    if(progress == undefined) {
+
+    if (progress == undefined) {
         progress = console.log;
-    };
-    
-    Seq()   
+    }
+    ;
+
+    Seq()
             // executing the assembly script that will build the object
             .seq(function () {
                 //exec("bash assembly.sh", this);
-                
+
                 //this();
                 //return;
-                
-                
-                
-                
-                var cmd = spawn("bash",["assembly.sh"])
-                
-                var displayOutput = function(output) {
+
+
+
+
+                var cmd = spawn("bash", ["assembly.sh"])
+
+                var displayOutput = function (output) {
                     progress(output.toString());
                 };
-                
-                cmd.stdout.on("data",displayOutput);
-                cmd.stderr.on("data",displayOutput);
-                
+
+                cmd.stdout.on("data", displayOutput);
+                cmd.stderr.on("data", displayOutput);
+
                 var cb = this;
-                
-                cmd.on("exit",function(code) {
-                   if(code == 0) {
-                       cb();
-                   } 
-                   else {
-                       cb(code);
-                   }
-                });                
+
+                cmd.on("exit", function (code) {
+                    if (code == 0) {
+                        cb();
+                    } else {
+                        cb(code);
+                    }
+                });
             })
             .catch(function (err, output) {
-                callback(err,output);
+                callback(err, output);
             })
-            .seq(function (err, stdout,stderr) {
+            .seq(function (err, stdout, stderr) {
                 response.stdout = stdout;
                 response.stderr = stderr;
-                
+
                 exec("bash rm jars/*");
-                
+
                 this();
             })
             // first we get the list of jars of the remote
@@ -94,9 +97,12 @@ module.exports = function (callback,progress) {
                 var toCopy = local;
                 updater.copyList(toCopy, config.pathToImageJFXDependencies, config.dependenciesDirectory);
 
-                updater.writeXMLFile("jars/", toCopy, "db.xml", this);
+                this();
 
             })
+            .seq(repo.read)
+            .seq(repo.checkCurrentJars)
+            .seq(repo.write)
             .seq(function () {
                 console.log("Build success");
                 callback(undefined);
