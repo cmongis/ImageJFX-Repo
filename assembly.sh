49429fbb34f3ddd185d9a5dbba835efea162843c
cyril <cyril@129.206.96.102>
2017-09-21 15:59:25 +0200

    The assembly script now takes care of the whole file management so the server just takes care of generating the db.xml.gz file

--- assembly.sh
+++ assembly.sh
@@ -1,13 +1,21 @@
-git clone https://github.com/cmongis/imagejfx-core || echo "Git repo already exists"
 
-echo "Going to ImageJ Folder<br>"
-cd imagejfx-core
+
+mkdir -p $1
+git clone https://github.com/cmongis/imagejfx-core || echo "Git repo already exists"
+echo "Going to ImageJ Folder $1"
+cd $1 || exit 3
+cd imagejfx-core || git clone http://github.com/cmongis/imagejfx-core.git
+cd imagejfx-core || echo ":-)"
 echo $(pwd)
 echo "<br>Pull modification<br>"
-git fetch origin
+git fetch origin || exit
 git reset --hard origin/master
 
 echo "<br>Deleting libraries"
 rm -Rfv target/*
 mvn -e assembly:assembly || mvn3 -e assembly:assembly
+mkdir -p ../jars
+rm ../jars/*
+mv -v target/imagejfx-core*bin/lib/* ../jars
+mv -v target/imagejfx-core*.jar ../jars
 exit
