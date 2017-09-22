413931cc8c37258322c3898424efbfe6fa7477ea
cyril <cyril@129.206.96.102>
2017-09-22 12:51:58 +0200

    Strip down to testing unitary funcion without big incidence

--- test.js
+++ test.js
@@ -14,24 +14,8 @@ var fs = require("fs");
 var Seq = require("seq");
 var zlib = require("zlib");
 var fspath = require("./path.js");
-
-console.log(fspath("/foo","bar"));
-console.log(fspath("/foo/","/bar"));
-console.log(fspath("/foo","/bar","hello.txt"));
-console.log(fspath("/foo","bar","/hello.txt"));
-
-
-var updater = require("./updater2.js");
-updater(function() {
-    console.log("it works");
-});
-
-/*
-repo    .read(function(db) {
-        
-        
-        //console.log(repo.getId("jars/ijfx-core-1.1.jar"));
-        //console.log(repo.getPlugin("imagejfx-core"));
-        repo.checkCurrentJars(repo.write);
-        //repo.write();
-});*/
+var config = require("./config.js");
+console.log(fspath("/foo", "bar"));
+console.log(fspath("/foo/", "/bar"));
+console.log(fspath("/foo", "/bar", "hello.txt"));
+console.log(fspath("/foo", "bar", "/hello.txt"));
